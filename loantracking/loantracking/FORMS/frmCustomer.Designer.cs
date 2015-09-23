651718e438590ea6be30b5eda9ae96b1fabd9f21
hammerj01 <hmertojr01@gmail.com>
2015-09-22 18:21:35 -0700

    money lender design
    adding fields in money lender

--- loantracking/loantracking/FORMS/frmCustomer.Designer.cs
+++ loantracking/loantracking/FORMS/frmCustomer.Designer.cs
@@ -46,13 +46,17 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.txtContactNo = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cboGender = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(18, 36);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(126, 16);
             this.label1.TabIndex = 1;
@@ -61,7 +65,7 @@
             // txtMoneyLenderID
             // 
             this.txtMoneyLenderID.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMoneyLenderID.Location = new System.Drawing.Point(150, 33);
+            this.txtMoneyLenderID.Location = new System.Drawing.Point(15, 28);
             this.txtMoneyLenderID.Name = "txtMoneyLenderID";
             this.txtMoneyLenderID.Size = new System.Drawing.Size(316, 23);
             this.txtMoneyLenderID.TabIndex = 2;
@@ -69,7 +73,7 @@
             // txtFname
             // 
             this.txtFname.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtFname.Location = new System.Drawing.Point(150, 62);
+            this.txtFname.Location = new System.Drawing.Point(15, 75);
             this.txtFname.Name = "txtFname";
             this.txtFname.Size = new System.Drawing.Size(316, 23);
             this.txtFname.TabIndex = 4;
@@ -78,7 +82,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(60, 65);
+            this.label2.Location = new System.Drawing.Point(12, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(84, 16);
             this.label2.TabIndex = 3;
@@ -87,7 +91,7 @@
             // txtMI
             // 
             this.txtMI.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMI.Location = new System.Drawing.Point(150, 91);
+            this.txtMI.Location = new System.Drawing.Point(337, 75);
             this.txtMI.MaxLength = 1;
             this.txtMI.Name = "txtMI";
             this.txtMI.Size = new System.Drawing.Size(41, 23);
@@ -97,7 +101,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(115, 94);
+            this.label3.Location = new System.Drawing.Point(334, 56);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 16);
             this.label3.TabIndex = 5;
@@ -106,7 +110,7 @@
             // txtLname
             // 
             this.txtLname.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtLname.Location = new System.Drawing.Point(150, 120);
+            this.txtLname.Location = new System.Drawing.Point(384, 75);
             this.txtLname.Name = "txtLname";
             this.txtLname.Size = new System.Drawing.Size(316, 23);
             this.txtLname.TabIndex = 8;
@@ -115,7 +119,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(62, 123);
+            this.label4.Location = new System.Drawing.Point(381, 56);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 16);
             this.label4.TabIndex = 7;
@@ -124,7 +128,7 @@
             // txtAddress
             // 
             this.txtAddress.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAddress.Location = new System.Drawing.Point(150, 160);
+            this.txtAddress.Location = new System.Drawing.Point(15, 125);
             this.txtAddress.Multiline = true;
             this.txtAddress.Name = "txtAddress";
             this.txtAddress.Size = new System.Drawing.Size(316, 96);
@@ -134,7 +138,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(73, 160);
+            this.label5.Location = new System.Drawing.Point(12, 106);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(71, 16);
             this.label5.TabIndex = 9;
@@ -143,7 +147,7 @@
             // txtAge
             // 
             this.txtAge.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAge.Location = new System.Drawing.Point(150, 262);
+            this.txtAge.Location = new System.Drawing.Point(337, 125);
             this.txtAge.Name = "txtAge";
             this.txtAge.Size = new System.Drawing.Size(41, 23);
             this.txtAge.TabIndex = 12;
@@ -152,7 +156,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(100, 265);
+            this.label6.Location = new System.Drawing.Point(334, 106);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(44, 16);
             this.label6.TabIndex = 11;
@@ -161,7 +165,7 @@
             // txtCreditLimit
             // 
             this.txtCreditLimit.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCreditLimit.Location = new System.Drawing.Point(150, 320);
+            this.txtCreditLimit.Location = new System.Drawing.Point(163, 371);
             this.txtCreditLimit.Name = "txtCreditLimit";
             this.txtCreditLimit.Size = new System.Drawing.Size(316, 23);
             this.txtCreditLimit.TabIndex = 14;
@@ -171,7 +175,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(51, 323);
+            this.label7.Location = new System.Drawing.Point(64, 374);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(93, 16);
             this.label7.TabIndex = 13;
@@ -200,7 +204,7 @@
             // txtContactNo
             // 
             this.txtContactNo.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContactNo.Location = new System.Drawing.Point(150, 291);
+            this.txtContactNo.Location = new System.Drawing.Point(387, 175);
             this.txtContactNo.Name = "txtContactNo";
             this.txtContactNo.Size = new System.Drawing.Size(316, 23);
             this.txtContactNo.TabIndex = 18;
@@ -210,17 +214,59 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(45, 294);
+            this.label8.Location = new System.Drawing.Point(384, 156);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(99, 16);
             this.label8.TabIndex = 17;
             this.label8.Text = "Contact No. :";
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(384, 124);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(316, 20);
+            this.dateTimePicker1.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(384, 105);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(103, 16);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Date of Birth :";
+            // 
+            // cboGender
+            // 
+            this.cboGender.FormattingEnabled = true;
+            this.cboGender.Items.AddRange(new object[] {
+            "Female",
+            "Male"});
+            this.cboGender.Location = new System.Drawing.Point(737, 75);
+            this.cboGender.Name = "cboGender";
+            this.cboGender.Size = new System.Drawing.Size(205, 21);
+            this.cboGender.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(734, 56);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 16);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Gender :";
+            // 
             // frmCustomer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 482);
+            this.ClientSize = new System.Drawing.Size(975, 517);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.cboGender);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.txtContactNo);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.btnSave);
@@ -267,5 +313,9 @@
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.TextBox txtContactNo;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cboGender;
+        private System.Windows.Forms.Label label10;
     }
 }
